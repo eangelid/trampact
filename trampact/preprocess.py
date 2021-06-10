f8d36019a17500c581202b1dcdde152e1f0db9c6
Evangelos Angelidis <eangelid@yahoo.co.uk>
2021-06-10 04:27:55 +0200

    Progress on preprocessing

--- trampact/preprocess.py
+++ trampact/preprocess.py
@@ -7,13 +7,34 @@ def get_data(file_name, is_local_csv=False):
     if is_local_csv:
         csv_path = os.path.join(os.path.dirname(__file__), 'data')
 
-        df_data = pd.read_csv(os.path.join(csv_path, file_name))
+        df_data = pd.read_csv(os.path.join(csv_path, file_name), index_col='iris_id')
     else:
         df_data = pd.read_csv(file_name)
         
     return df_data
 
-
+def drop_tx_columns(data, year):
+    return data.drop(columns=[
+        f'tx_chom_{year}'
+        ,f'tx_empl_{year}'
+        ,f'tx_ouvr_{year}'
+        ,f'tx_TP_{year}'
+        ,f'tx_HLM_{year}'
+        ,f'tx_no_transp_{year}'
+        ,f'tx_walk_{year}'
+        ,f'tx_moto_{year}'
+        ,f'tx_voit_{year}'
+        ,f'tx_TC_{year}'
+        ,f'tx_HH_moins2ans_{year}'
+        ,f'tx_HH_2_4ans_{year}'
+        ,f'tx_HH_5_9ans_{year}'
+        ,f'tx_HH_plus10ans_{year}'
+        ,f'tx_HH_with_park_{year}'
+        ,f'tx_HH_with_voit_{year}'
+        ,f'tx_HH_1voit_{year}'
+        ,f'tx_HH_2voit_{year}'
+        ,f'tx_empl_prec_{year}'
+    ])    
 
 if __name__ == "__main__":
     dbx_url = "https://www.dropbox.com/s/r6kzjdo6l6qbdvg/BD_GENT_2006.csv?dl=1"
