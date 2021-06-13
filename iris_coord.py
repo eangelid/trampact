6e1e8cc9c6350ac783add54e513727e317f30cd1
Marie Chistyakova <mallliya@hotmail.com>
2021-06-13 17:27:10 +0200

    add iris coordinates package

--- iris_coord.py
+++ iris_coord.py
@@ -1,12 +1,8 @@
 import shapefile as sp
 from dbfread import DBF
 
-''' To access data download two files from iris_geo directory on Google Storage
-
-    * Use get_box_coord(iris_id) to get two points of iris to obtain a box
-    * Use get_poly_coord(iris_id) to get a polygone to obtain exact borders of an iris
-
-    - To get the correspondance of iris_id and iris_type use get_iris_type('iris_id')
+''' ! Please download two files from gs://data/iris_geo in order to use the package
+    ! iris_id has 9 digits
 '''
 
 class Iris():
@@ -16,6 +12,8 @@ class Iris():
         self.get_iris_id()
 
     def get_files(self):
+      '''Read data from locally saved files'''
+
         path_dbf = '../raw_data/iris_geo/IRIS_GEO_2018_FRTOT.dbf'
         path_shp = '../raw_data/iris_geo/IRIS_GEO_2018_FRTOT.shp'
 
@@ -23,32 +21,34 @@ class Iris():
         self.shp = sp.Reader(path_shp)
 
     def get_records(self):
+      '''Get a list of dictionaries with coordinates data'''
+
         records = []
         for record in self.dbf:
             records.append(dict(record))
         self.records = records
 
     def get_iris_id(self):
+      '''Create a list of all iris_ids of France'''
+
         iris_id = []
         for i in range(len(self.records)):
             r = self.records[i]['CODE_IRIS']
             iris_id.append(r)
         self.iris_id = iris_id
+
     def get_iris_type(self, iris_id):
+      '''Get the type of iris (H, A, Z) given iris_id'''
+
         iris_type = {}
         for i in range(len(self.records)):
             iris_type[self.iris_id[i]]= self.records[i]['TYP_IRIS']
         self.iris_type = iris_type
         return iris_type[str(iris_id)]
 
-    def get_iris_type(self):
-        iris_id = []
-        for i in range(len(self.records)):
-            r = self.records[i]['TYP_IRIS']
-            iris_id.append(r)
-        self.iris_id = iris_id
-
     def get_box_coord(self, iris_id):
+      '''Get approximative iris coordinates in a rectangular form'''
+
         shapes = self.shp.shapes()
         box_coord = {}
         for i in range(len(shapes)):
@@ -56,9 +56,18 @@ class Iris():
         return box_coord[str(iris_id)]
 
     def get_poly_coord(self, iris_id):
+      '''Get exact iris coordinates in a polynomial form'''
         shapes = self.shp.shapes()
         poly_coord = {}
         for i in range(len(shapes)):
             poly_coord[self.iris_id[i]] = shapes[i].__geo_interface__[
                 'coordinates'][0]
         return poly_coord[str(iris_id)]
+
+    def get_iris_list(self):
+      '''Get the list of all iris_ids of France'''
+        iris_id = []
+        for i in range(len(records)):
+            r = records[i]['CODE_IRIS']
+            iris_id.append(r)
+        return iris_id
